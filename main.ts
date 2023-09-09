6ee14f0bffc93b30bf308ac03358459e111a7593
Cheyoon Jung <jessietwo94@gmail.com>
2023-09-09 18:37:23 +0200

    fist commit

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 
 import puppeteer from 'puppeteer';
+import 'dotenv/config'
 
 
 (async () => {
@@ -8,7 +9,7 @@ import puppeteer from 'puppeteer';
   });
   // Create a new page
   const page = await browser.newPage();
-  await page.goto('https://sooperang.or.kr/main.do', {
+  await page.goto(process.env.SITE_URL ?? '', {
     waitUntil: 'load',
     // Remove the timeout
     timeout: 0
@@ -22,21 +23,10 @@ import puppeteer from 'puppeteer';
   //reservation: 원하는 날짜 바꾸려면 data-title 값을 바꿔야한다
   const dayFrom = 'td.weekend.available span[data-title="r1c6"]';
   const dayTo = 'td.weekend.available span[data-title="r2c0"]';
-
   const chooseLocationBtn = 'button#selectInstt';
   const location = 'li.qrInsttClass#qrInstt_FT00001';
   const product = 'button#selectProduct';
   const search = 'button.btn[onclick*="fn_formSubmit()"]';
-
-// 예약가능 체크용, 예약가능한 스트링이 진짜 '예약가능'이여야만 함..확인할 것
-  const isReservable = await page.evaluate(() => {
-    const liElement = document.querySelector('li .pop_in2_inner1.sb4');
-    if (liElement) {
-      const spanText = liElement.querySelector('span')?.textContent;
-      return spanText?.includes('예약가능') || false;
-    }
-    return false;
-  });
 
 
   await page.waitForSelector(dayFrom);
@@ -56,14 +46,19 @@ import puppeteer from 'puppeteer';
   await page.click('li[onclick^="fn_selectProduct(1,"]');
   await page.waitForSelector(search);
   await page.click(search)
-
+  await page.waitForNavigation({ waitUntil: 'load' });
   //check reservation
-  if (isReservable) {
-    console.log('예약가능');
-  } else {
-    console.log('예약불가');
+  const isReservable = await page.$eval(
+  '#divCnt1 > div.popup.sb4 > div > div.pop_in2.sb4 > ul > li:nth-child(1)', element => {
+    return element.textContent?.trim().toLowerCase() === '예약불가';
+  });
 
+  if (isReservable) {
+    console.log('예약불가');
+  } else {
+    console.log('예약가능');
   }
   //Close browser
   await browser.close();
 })();
+
